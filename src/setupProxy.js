fc875b5e008ee70c489e08fa91984a566958fb58
Jaakko Heusala <jhh@hg.fi>
2024-08-21 07:57:01 +0300

    added finnish translation (https://github.com/hyperifyio/project-govm/issues/35)

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2024. Sendanor <info@sendanor.fi>. All rights reserved.
 
-const REACT_APP_BACKEND_TARGET_URL = process?.env?.REACT_APP_BACKEND_TARGET_URL ?? 'http://localhost:3001';
+const REACT_APP_BACKEND_TARGET_URL = process?.env?.REACT_APP_BACKEND_TARGET_URL ?? 'https://localhost:3001';
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
@@ -12,6 +12,7 @@ module.exports = function(app) {
             changeOrigin: true,
             ws: true,
             autoRewrite: true,
+            secure: false, // Verify SSL disabled for development
             // logLevel: 'debug',
             //protocolRewrite: 'http',
             // pathRewrite: {
